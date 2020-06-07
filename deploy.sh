67d8f700e82d22b52e751f14c8d77466d5a16adf
Ghanesh Balaji <ghanesh.balaji1995@gmail.com>
2020-06-07 21:21:39 +0530

    Test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 cat creds.txt | docker login -u ${CI_REGISTRY_USER} --password-stdin
-mkdir -p app && cd app
 docker pull shreegb/docker_deploy_test
 docker container stop sample-api
 docker container rm sample-api
