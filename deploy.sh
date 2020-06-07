922a30e44062e21dee8c1267653c0eeef4cb74aa
Ghanesh Balaji <ghanesh.balaji1995@gmail.com>
2020-06-07 18:58:06 +0530

    Test

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+cat creds.txt | docker login -u ${CI_REGISTRY_USER} --password-stdin
+mkdir -p app && cd app
+docker pull shreegb/docker_deploy_test
+docker container run -p 8080:8080 -d shreegb/docker_deploy_test
